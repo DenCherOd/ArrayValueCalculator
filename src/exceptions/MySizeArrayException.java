ab76a8747fd132348272f2762a2d7e2e633f8aae
Denys Cheran <dencherod@gmail.com>
2022-04-05 18:14:31 +0300

    final commit

--- src/exceptions/MySizeArrayException.java
+++ src/exceptions/MySizeArrayException.java
@@ -1,8 +1,7 @@
 package exceptions;
 
 public class MySizeArrayException extends Exception {
-    public String toString()
-    {
-        return "Error. Array size isn`t 4.";
+    public MySizeArrayException(String str) {
+        super(str);
     }
 }
